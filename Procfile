0f0d50a3ff1889323f99249701efa24c095030ec
akarisch <akarisch@chicagobooth.edu>
2020-12-07 20:39:28 -0600

    added more comments

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+
 web: gunicorn wine_api.wsgi
--- wine_api/urls.py
+++ wine_api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# urls for the project
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wines/', include ('wines_app.urls'))
